d489a859a73f46dc05b2592f2fb84ccedf4d3e80
marciliodevjava <marciliodevjava@gmail.com>
2025-08-28 11:43:26 -0300

    Update CustomerDto

--- src/main/kotlin/br/com/marcilio/mercado_livro/dto/request/CustomerDto.kt
+++ src/main/kotlin/br/com/marcilio/mercado_livro/dto/request/CustomerDto.kt
@@ -1,3 +1,3 @@
 package br.com.marcilio.mercado_livro.dto.request
 
-data class CustomerDto(val id: Long, val name:String, val email: String)
+data class CustomerDto(val name:String, val email: String)
