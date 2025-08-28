d30cab52ac9fe9a15e10c8696ee2e5f33ece3b8a
marciliodevjava <marciliodevjava@gmail.com>
2025-08-28 11:43:52 -0300

    Update CustomerServiceImpl

--- src/main/kotlin/br/com/marcilio/mercado_livro/service/impl/CustomerServiceImpl.kt
+++ src/main/kotlin/br/com/marcilio/mercado_livro/service/impl/CustomerServiceImpl.kt
@@ -8,19 +8,28 @@ import org.springframework.stereotype.Service
 @Service
 class CustomerServiceImpl : CustomerService {
     open lateinit var list: MutableList<CustomerResponseDto>
-
+    var genNum: Long = 1;
     init {
         list = mutableListOf()
     }
 
     override fun criar(customer: CustomerDto): CustomerResponseDto {
-        val dados: CustomerResponseDto = CustomerResponseDto(customer.id, customer.name, customer.email)
+        val dados: CustomerResponseDto = CustomerResponseDto(genNum,customer.name, customer.email)
         list.add(dados)
+        number()
         return dados
     }
 
     override fun findById(iden: Long): CustomerResponseDto {
         val customer: CustomerResponseDto = list.first { it.id == iden } as CustomerResponseDto
         return customer
+    }
+
+    override fun findAll(): MutableList<CustomerResponseDto>{
+        return list
+    }
+
+    fun number(){
+        genNum++
     }
 }
