6d9ad4b4175efa4e21b0f37bbe0fc03083d5a4e4
marciliosilvazup <marcilio.silva@zup.com.br>
2025-08-28 10:20:25 -0300

    Create class CustomerService

---
+++ src/main/kotlin/br/com/marcilio/mercado_livro/service/CustomerService.kt
@@ -0,0 +1,9 @@
+package br.com.marcilio.mercado_livro.service
+
+import br.com.marcilio.mercado_livro.dto.request.CustomerDto
+import br.com.marcilio.mercado_livro.dto.response.CustomerResponseDto
+
+interface CustomerService {
+    fun criar(customer: CustomerDto): CustomerResponseDto
+    fun findById(iden: Long): CustomerResponseDto
+}
