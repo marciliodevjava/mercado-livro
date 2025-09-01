83863d00d923fea26e227fa5c42450f9d5f2eca6
marciliodevjava <marciliodevjava@gmail.com>
2025-09-01 18:17:01 -0300

    Update CustomerModel

--- src/main/kotlin/br/com/marcilio/mercado_livro/model/CustomerModel.kt
+++ src/main/kotlin/br/com/marcilio/mercado_livro/model/CustomerModel.kt
@@ -1,7 +1,18 @@
 package br.com.marcilio.mercado_livro.model
 
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity(name = "customer")
 data class CustomerModel(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
+    @Column(name="name")
     var name: String,
+    @Column(name = "email")
     var email: String
 )
