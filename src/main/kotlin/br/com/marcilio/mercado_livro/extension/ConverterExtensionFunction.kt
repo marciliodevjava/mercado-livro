865893b2bc8efb501ffa8d8eb3081b20de977f34
marciliodevjava <marciliodevjava@gmail.com>
2025-09-01 18:17:13 -0300

    Update ConverterExtensionFunction.kt

--- src/main/kotlin/br/com/marcilio/mercado_livro/extension/ConverterExtensionFunction.kt
+++ src/main/kotlin/br/com/marcilio/mercado_livro/extension/ConverterExtensionFunction.kt
@@ -1,8 +1,14 @@
 package br.com.marcilio.mercado_livro.extension
 
 import br.com.marcilio.mercado_livro.dto.request.CustomerDto
+import br.com.marcilio.mercado_livro.dto.response.CustomerResponseDto
 import br.com.marcilio.mercado_livro.model.CustomerModel
 
-fun CustomerDto.toCustomerModel(): CustomerModel{
+
+fun CustomerDto.toCustomerModel(): CustomerModel {
     return CustomerModel(name = this.name, email = this.email)
+}
+
+fun CustomerModel.toCustomerModel(): CustomerResponseDto {
+    return CustomerResponseDto(id = this.id!!, name = this.name, email = this.email)
 }
